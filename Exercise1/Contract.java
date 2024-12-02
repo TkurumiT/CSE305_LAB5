1f7e721d97de244b8e5945816aa20b8a1bd5590c
TkurumiT <155941886+TkurumiT@users.noreply.github.com>
2024-12-02 17:22:14 +0700

    Delete Exercise1 directory

--- Exercise1/Contract.java
+++
@@ -1,9 +0,0 @@
-package Assignment5;
-
-public interface Contract {
-    void buildContractID(String contractID);
-    void buildPropertyID(String propertyID);
-    void buildTenantID(String tenantID);
-    void buildRentAmount(double rentAmount);
-    String signContract();
-}
